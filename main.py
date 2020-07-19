e6c41e4226ca4e6159734288ee87743d0b2641ca
Krzysztof Wojdat <kwojdat@hugeinc.com>
2020-07-19 14:44:05 -0500

    Simple flask trace with Cloud Trace Export

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 import datetime
+import time
 
 from flask import Flask, render_template
 
 # tracing.py
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
-)
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+
+
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(ConsoleSpanExporter())
+    SimpleExportSpanProcessor(CloudTraceSpanExporter())
 )
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def root():
     with tracer.start_as_current_span("root"):
         print("Hello world from OpenTelemetry Python!")
+        time.sleep(1)
         foo("going deeper", "underdark")
         return "It works!"
 
 
 def foo(value1, value2):
     with tracer.start_as_current_span(f"Span for foo: {value2}"):
+        time.sleep(1)
         print(f"value1: {value1}")
         bar("going even deeper", "moria")
 
 
 def bar(value1, value2):
     with tracer.start_as_current_span(f"Span for bar {value2}"):
+        time.sleep(2)
         print(f"value1: {value1}")
 
 
